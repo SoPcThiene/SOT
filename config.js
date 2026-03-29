a45701bab266190f2b743c3f1ee1d8e44ee4267e
SoPcThiene <so.pcthiene@gmail.com>
2026-03-29 08:56:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ window.APP_CONFIG = {
     
     // 2. Incolla qui ESATTAMENTE il blocco di Firebase
     firebaseSettings: {
-  "apiKey": "AIzaSyDMcnP_3TD1h9fYobsN6PN5aNGDGHe1y0c",
-  "authDomain": "so-pcthiene.firebaseapp.com",
-  "databaseURL": "https://so-pcthiene-default-rtdb.europe-west1.firebasedatabase.app",
-  "projectId": "so-pcthiene",
-  "storageBucket": "so-pcthiene.firebasestorage.app",
-  "messagingSenderId": "383344731618",
-  "appId": "1:383344731618:web:5bb0f9b30d100381bc85bb"
+  apiKey: "AIzaSyDMcnP_3TD1h9fYobsN6PN5aNGDGHe1y0c",
+  authDomain: "so-pcthiene.firebaseapp.com",
+  databaseURL: "https://so-pcthiene-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "so-pcthiene",
+  storageBucket: "so-pcthiene.firebasestorage.app",
+  messagingSenderId: "383344731618",
+  appId: "1:383344731618:web:5bb0f9b30d100381bc85bb"
 }
 };
 
